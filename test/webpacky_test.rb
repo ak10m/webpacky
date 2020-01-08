7f889563b8b5a599556f54efd85bccff9950b468
ak10m <akio.morimoto@airits.jp>
2020-01-08 14:44:01 +0900

    add minitest-reporter gem

--- test/webpacky_test.rb
+++ test/webpacky_test.rb
@@ -6,8 +6,4 @@ class WebpackyTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Webpacky::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
